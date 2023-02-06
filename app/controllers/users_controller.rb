aa4ad3d69259d5234e2b42e37ebc7a1df159bc1b
PlinaW <plinski.wojciech@gmail.com>
2023-02-06 22:14:06 +0100

    adding-update-feature

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -12,6 +12,10 @@ class UsersController < ApplicationController
     @user = User.new
   end
 
+  def edit
+    @user = User.find(params[:id])
+  end
+
   def create
     @user = User.new(user_params)
     if @user.save
@@ -19,6 +23,16 @@ class UsersController < ApplicationController
       redirect_to users_path
     else
       render 'new'
+    end
+  end
+
+  def update
+    @user = User.find(params[:id])
+    if @user.update(user_params)
+      flash[:notice] = "Your profile was updated successfully"
+      redirect_to @user
+    else
+      render 'edit'
     end
   end
 
