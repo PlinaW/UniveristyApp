f7b88d056cd87acf1a490890e2b8e172c867c364
PlinaW <plinski.wojciech@gmail.com>
2023-02-07 22:43:15 +0100

    adding-form-password-update

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -43,7 +43,7 @@ class UsersController < ApplicationController
   private
 
   def user_params
-    params.require(:user).permit(:name, :email)
+    params.require(:user).permit(:name, :email, :password, :password_confirmation)
   end
 
   def set_user
