e457288ebf081b9ee68f8d8cab1bbb8ab9865ebd
PlinaW <plinski.wojciech@gmail.com>
2023-02-09 22:01:55 +0100

    adding-loggin-feature

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -5,11 +5,21 @@ class SessionsController < ApplicationController
     end
 
     def create
-        
+        user = User.find_by(email: params[:session][:email])
+        if user && user.authenticate(params[:session][:password])
+            session[:user_id] = user.id
+            flash[:notice] = "You have successfully logged in"
+            redirect_to user    
+        else
+            flash.now[:alert] = "Somthing was wrong with your loggin information"
+            render 'new'
+        end
     end
 
     def destroy
-    
+        session[:user_id] = nil
+        flash[:notice] = "You have successfully logged out"
+        redirect_to root_path
     end
 
     
